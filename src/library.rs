f814b7fc5ebdb9ca9b0a5abff0db4a8dadf4643b
Kris Tokarz <kj.tokarz@gmail.com>
2019-07-13 11:05:41 -0400

    creating a simple librarian app in rust

---
+++ src/library.rs
@@ -0,0 +1,6 @@
+pub mod lib {
+    pub struct Librarian {
+        pub books_available: Vec<String>,
+        pub number_of_books: i32,
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+mod library;
+
+fn main() {
+    println!("Hello, enter a book! I will see if we have it");
+
+    let mut book = String::new();
+    io::stdin()
+        .read_line(&mut book)
+        .expect("Entered invalid input");
+
+    let l = library::lib::Librarian {
+        books_available: Vec::new(),
+        number_of_books: 1,
+    };
+}
